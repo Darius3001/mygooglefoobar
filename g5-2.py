7d36069f9f1d4404f4dbbc4fef3984f5264f4b5a
darius <darius.mertin@outlook.com>
2023-10-17 12:59:44 +0200

    some bug

--- g5-2.py
+++ g5-2.py
@@ -11,16 +11,31 @@ def solution(g):
     
     possible_columns = itertools.product([True,False], repeat=columnlen+1)
     
-    
-def isRowValid(generatedPostRow, previousPastRow, currentRow):
-    for i in range(len(currentRow)):
-        x00 = previousPastRow[i]
-        x01 = previousPastRow[i+1]
-        x10 = generatedPostRow[i]
-        x11 = generatedPostRow[i+1]
+    previous_columns = {col:1 for col in possible_columns}
+
+    for i in range(rowlen):
+        current_columns = {}
+        for column in possible_columns:
+            for previous_column, previous_n in previous_columns.iteritems():
+                if isColumnValid(column, previous_column, g[i]):
+                    new_n = previous_n
+                    new_n += current_columns[column] if column in current_columns else 0
+                    current_columns[column] = new_n
         
-        if (sum([x00, x01, x10, x11]) == 1) != currentRow[i]:
+        previous_columns = current_columns
+        
+    return sum(previous_columns.values())
+                
+    
+def isColumnValid(generatedPostColumn, previousPastColumn, currentColumn):
+    for i in range(len(currentColumn)):
+        x00 = previousPastColumn[i]
+        x01 = previousPastColumn[i+1]
+        x10 = generatedPostColumn[i]
+        x11 = generatedPostColumn[i+1]
+        
+        if (sum([x00, x01, x10, x11]) == 1) != currentColumn[i]:
             return False
     return True
 
-solution([[True,False,True],[False,True,False],[True,False,True]])
+print(solution([[True,False,True],[False,True,False],[True,False,True]]))
