de25213b513cddc242858be3d20782b23b3f9e93
darius <darius.mertin@outlook.com>
2023-10-17 12:39:17 +0200

    bruteforce approach

--- g5-2.py
+++ g5-2.py
@@ -1,29 +1,26 @@
-"""
-1. precalc
-
-2. build columns and count them
-
-"""
-
-class BlockCalculator:
-  def __init__(self) -> None:
-    # TODO fill precalc
-    self.precalc = {}
-  
-  def possiblePastBlocks(self, currentBlock, previousBlock=[[None, None],[None,None]]):
-    #TODO
-    pass
-
-class ColumnCalculator:
-  def __init__(self) -> None:
-    pass
-
-  def possiblePastColumns(self, currentColumn, previousColumn=None):
-    if previousColumn is None:
-      # first column
-      pass
-    pass
+import numpy as np
+import itertools
 
 def solution(g):
-  precalc = BlockCalculator()
-  
+    # Transpose because columns are smaller
+    # matrix will be viewed as list of columns (not rows)
+    g = np.array(g).T
+    
+    rowlen = len(g)
+    columnlen = len(g[0])
+    
+    possible_columns = itertools.product([True,False], repeat=columnlen+1)
+    
+    
+def isRowValid(generatedPostRow, previousPastRow, currentRow):
+    for i in range(len(currentRow)):
+        x00 = previousPastRow[i]
+        x01 = previousPastRow[i+1]
+        x10 = generatedPostRow[i]
+        x11 = generatedPostRow[i+1]
+        
+        if (sum([x00, x01, x10, x11]) == 1) != currentRow[i]:
+            return False
+    return True
+
+solution([[True,False,True],[False,True,False],[True,False,True]])
