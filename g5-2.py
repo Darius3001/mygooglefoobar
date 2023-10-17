4a3c77c598b29d6d332c507178d4e0262a42a073
darius <darius.mertin@outlook.com>
2023-10-17 13:44:28 +0200

    more readable

--- g5-2.py
+++ g5-2.py
@@ -1,6 +1,8 @@
 import numpy as np
 import itertools
 
+
+
 def solution(g):
     # Transpose because columns are smaller
     # matrix will be viewed as list of columns (not rows)
@@ -10,32 +12,41 @@ def solution(g):
     
     possible_columns = list(itertools.product([True,False], repeat=columnlen+1))
     
-    solutioncount_previous_columns = {col:1 for col in possible_columns}
+    solutioncount_columns = {col:1 for col in possible_columns}
 
     for i in range(rowlen):
+        solutioncount_columns = nextColumnSolutionCount(
+            solutioncount_columns,
+            g[i],
+            possible_columns
+        )
         
-        solutioncount_current_columns = {}
-        
-        for column in possible_columns:
-            
-            for previous_column, previous_n in solutioncount_previous_columns.iteritems():
+    return sum(solutioncount_columns.values())
                 
-                if isColumnValid(column, previous_column, g[i]):
-                    
-                    new_n = previous_n
-                    new_n += solutioncount_current_columns[column] if column in solutioncount_current_columns else 0
-                    
-                    solutioncount_current_columns[column] = new_n
-                    
-        solutioncount_previous_columns = solutioncount_current_columns
-        
-    return sum(solutioncount_previous_columns.values())
-                
+
+def nextColumnSolutionCount(solutioncount_previous_columns, currentColumn, possible_columns):
     
-def isColumnValid(generatedPostColumn, previousPastColumn, currentColumn):
+    result = {}
+    
+    for column in possible_columns:
+        
+        for previous_column, previous_n in solutioncount_previous_columns.iteritems():
+            
+            if isColumnValid(column, previous_column, currentColumn):
+                
+                new_n = previous_n
+                new_n += result[column] \
+                    if column in result else 0
+                
+                result[column] = new_n
+                
+    return result
+
+def isColumnValid(generatedPastColumn, previousPastColumn, currentColumn):
+    
     for i in range(len(currentColumn)):
         
-        boxSum = sum(generatedPostColumn[i:i+2]+previousPastColumn[i:i+2])
+        boxSum = sum(generatedPastColumn[i:i+2]+previousPastColumn[i:i+2])
         
         if (boxSum == 1) != currentColumn[i]:
             return False
